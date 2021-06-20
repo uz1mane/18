07331c20295f152a94941bb83d69b412ff6885c5
uz1mane <64533384+uz1mane@users.noreply.github.com>
2021-06-20 23:49:32 +0700

    Done.
    Done.

---
+++ immod18/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace immod18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.windowsUpDown = new System.Windows.Forms.NumericUpDown();
+            this.servedCustBox = new System.Windows.Forms.TextBox();
+            this.queueBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.windowsUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(222, 426);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(266, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Amount of windows:";
+            // 
+            // windowsUpDown
+            // 
+            this.windowsUpDown.Location = new System.Drawing.Point(399, 27);
+            this.windowsUpDown.Name = "windowsUpDown";
+            this.windowsUpDown.Size = new System.Drawing.Size(120, 22);
+            this.windowsUpDown.TabIndex = 2;
+            // 
+            // servedCustBox
+            // 
+            this.servedCustBox.Enabled = false;
+            this.servedCustBox.Location = new System.Drawing.Point(688, 27);
+            this.servedCustBox.Name = "servedCustBox";
+            this.servedCustBox.Size = new System.Drawing.Size(100, 22);
+            this.servedCustBox.TabIndex = 3;
+            // 
+            // queueBox
+            // 
+            this.queueBox.Enabled = false;
+            this.queueBox.Location = new System.Drawing.Point(554, 115);
+            this.queueBox.Name = "queueBox";
+            this.queueBox.Size = new System.Drawing.Size(100, 22);
+            this.queueBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(551, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Customers served:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(396, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Customers in queue:";
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(379, 212);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(75, 23);
+            this.startBtn.TabIndex = 7;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(602, 212);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(75, 23);
+            this.stopBtn.TabIndex = 8;
+            this.stopBtn.Text = "Stop";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.queueBox);
+            this.Controls.Add(this.servedCustBox);
+            this.Controls.Add(this.windowsUpDown);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.windowsUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown windowsUpDown;
+        private System.Windows.Forms.TextBox servedCustBox;
+        private System.Windows.Forms.TextBox queueBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ immod18/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace immod18
+{
+    public partial class Form1 : Form
+    {
+        public int windowNumber;
+        public int customersInQueueNumber;
+        public int customersServed;
+        Random rnd = new Random();
+        public double l = 200, m = 80;
+        public Form1()
+        {
+            InitializeComponent();
+            customersInQueueNumber = 0;
+            customersServed = 0;
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            windowsUpDown.Enabled = false;
+            windowNumber = (int)windowsUpDown.Value;
+            customersServed = windowNumber;
+            textBox1.Text = "";
+
+            timer1.Start();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            servedCustBox.Text = customersServed.ToString();
+            queueBox.Text = customersInQueueNumber.ToString();
+            double theory = countValues(l);
+            textBox1.Text += "Customers served: " + customersServed + Environment.NewLine;
+            double discrete = countValues(m * customersServed);
+            textBox1.Text += "Queue: " + customersInQueueNumber + Environment.NewLine;
+            textBox1.Text += "N: " + windowNumber + Environment.NewLine + "Theory: " + theory + Environment.NewLine + "Discrete: " + discrete + Environment.NewLine;
+            if(theory > discrete)
+            {
+                if (customersServed >= windowNumber)
+                {
+                    customersInQueueNumber++;
+                } 
+                else 
+                {
+                    customersServed++;
+                }
+            } 
+            else
+            {
+                if (customersInQueueNumber == 0)
+                {
+                    if (customersServed > 0)
+                    {
+                        customersServed--;
+                    }
+                }
+                else
+                {
+                    if (customersInQueueNumber > 0)
+                    {
+                        customersInQueueNumber--;
+                    }
+                }
+            }
+
+
+        }
+
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            servedCustBox.Text = "";
+            queueBox.Text = "";
+            windowsUpDown.Enabled = true;
+            textBox1.Text += "Stop" + Environment.NewLine;
+            customersServed = 0;
+            customersInQueueNumber = 0;
+        }
+
+        private double countValues(double n)
+        {
+            return n * Math.Exp(n * rnd.NextDouble() * (-1));
+        }
+    }
+}
